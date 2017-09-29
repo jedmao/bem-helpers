f5c7045a25b7516934152116c8195cf362b07013
Jed Mao <jedmao@outlook.com>
2017-09-29 15:07:37 -0700

    v0.4.0 - Defer resolveBEMModifiers to truthyStringsKeys

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,6 @@ import test from 'ava'
 import {
 	joinBEMElement,
 	joinBEMModifiers,
-	resolveBEMModifiers,
 	toBEMClassNames,
 } from './'
 
@@ -59,105 +58,6 @@ test('joinBEMModifiers joins two modifiers to the same block or element', t => {
 	t.deepEqual(
 		joinBEMModifiers('foo', ['bar', 'baz']),
 		['foo', 'foo--bar', 'foo--baz'],
-	)
-})
-
-test('resolveBEMModifiers returns an empty array by default', t => {
-	t.deepEqual(
-		resolveBEMModifiers(),
-		[],
-	)
-})
-
-test('resolveBEMModifiers returns input string wrapped in an array', t => {
-	t.deepEqual(
-		resolveBEMModifiers('foo'),
-		['foo'],
-	)
-})
-
-test('resolveBEMModifiers splits input string by multiple spaces and newlines', t => {
-	t.deepEqual(
-		resolveBEMModifiers(' \n  foo \r\n  bar   '),
-		['foo', 'bar'],
-	)
-})
-
-test('resolveBEMModifiers returns a flat array from a nested string array', t => {
-	t.deepEqual(
-		resolveBEMModifiers([
-			'foo',
-			['bar', [
-				'baz', [
-					'qux',
-				],
-			]],
-		]),
-		['foo', 'bar', 'baz', 'qux'],
-	)
-})
-
-test('resolveBEMModifiers returns a list of keys for which their values are truthy', t => {
-	t.deepEqual(
-		resolveBEMModifiers({
-			foo: true,
-			bar: false,
-			baz: 42,
-			qux: 0,
-		}),
-		['foo', 'baz'],
-	)
-})
-
-test('resolveBEMModifiers returns unique values', t => {
-	t.deepEqual(
-		resolveBEMModifiers(['foo', 'bar', 'foo']),
-		['foo', 'bar'],
-	)
-})
-
-test('resolveBEMModifiers omits falsey and non-string values', t => {
-	const values = [
-		'',
-		[],
-		'foo',
-		true,
-		false,
-		42,
-		undefined,
-		void 0,
-	]
-	t.deepEqual(
-		// tslint:disable-next-line:no-any
-		resolveBEMModifiers(values as any),
-		['foo'],
-	)
-})
-
-test('resolveBEMModifiers returns a simple flat array from a complex nested structure', t => {
-	const nested = [
-		'foo', [
-			{
-				bar: true,
-				baz: null,
-			},
-		],
-		'qux',
-		[
-			[
-				[
-					{
-						corge: undefined,
-						garpley: -1,
-					},
-				],
-			],
-		],
-	]
-	t.deepEqual(
-		// tslint:disable-next-line:no-any
-		resolveBEMModifiers(nested as any),
-		['foo', 'bar', 'qux', 'garpley'],
 	)
 })
 
